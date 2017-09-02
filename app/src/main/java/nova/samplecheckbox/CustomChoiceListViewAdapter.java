3730ca7455e9c0506ee9e2da88c663c3ad2e67f3
jamsya <jamsya@naver.com>
2017-09-02 17:49:20 +0900

    멀티 / 싱글모드 전환 가능. 삭제

--- app/src/main/java/nova/samplecheckbox/CustomChoiceListViewAdapter.java
+++ app/src/main/java/nova/samplecheckbox/CustomChoiceListViewAdapter.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 
 public class CustomChoiceListViewAdapter extends BaseAdapter {
     // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
-    private ArrayList<ListViewItem> listViewItemList = new ArrayList<ListViewItem>() ;
+    ArrayList<ListViewItem> listViewItemList = new ArrayList<ListViewItem>() ;
 
     // ListViewAdapter의 생성자
     public CustomChoiceListViewAdapter() {
--- app/src/main/java/nova/samplecheckbox/MainActivity.java
+++ app/src/main/java/nova/samplecheckbox/MainActivity.java
@@ -3,10 +3,16 @@ package nova.samplecheckbox;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listview;
+    CustomChoiceListViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        ListView listview;
-        CustomChoiceListViewAdapter adapter;
 
         // Adapter 생성
         adapter = new CustomChoiceListViewAdapter();
@@ -35,17 +39,53 @@ public class MainActivity extends AppCompatActivity {
                 "Assignment Ind Black 36dp");
 
 
+        // delete button에 대한 이벤트 처리.
+        Button deleteButton = (Button) findViewById(R.id.buttonDelete);
+        deleteButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                SparseBooleanArray checkedItems = listview.getCheckedItemPositions();
+                int count = adapter.getCount();
+
+                for (int i = count - 1; i >= 0; i--) {
+                    if (checkedItems.get(i)) {
+                        adapter.listViewItemList.remove(i);
+                    }
+                }
+
+                // 모든 선택 상태 초기화.
+                listview.clearChoices();
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
+        // selectAll button에 대한 이벤트 처리.
+        Button selectAllButton = (Button)findViewById(R.id.buttonSelectAll) ;
+        selectAllButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                int count = 0 ;
+                count = adapter.getCount() ;
+
+                for (int i=0; i<count; i++) {
+                    listview.setItemChecked(i, true) ;
+                }
+            }
+        }) ;
+
+
+
     }
+
     public void onClick(View v) {
-        switch(v.getId()){
+        switch (v.getId()) {
             case (R.id.buttonSwitchMode):
                 ListView listView = (ListView) findViewById(R.id.listview1);
-                if (listView.getChoiceMode() == ListView.CHOICE_MODE_MULTIPLE)
-                {
+                if (listView.getChoiceMode() == ListView.CHOICE_MODE_MULTIPLE) {
+                    Toast.makeText(getApplicationContext(), "단일 선택 모드로 변경되었습니다.", Toast.LENGTH_SHORT).show();
                     listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-                }
-                else if (listView.getChoiceMode() == ListView.CHOICE_MODE_SINGLE)
-                {
+                } else if (listView.getChoiceMode() == ListView.CHOICE_MODE_SINGLE) {
+                    Toast.makeText(getApplicationContext(), "다중 선택 모드로 변경되었습니다.", Toast.LENGTH_SHORT).show();
                     listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
                 }
                 break;
